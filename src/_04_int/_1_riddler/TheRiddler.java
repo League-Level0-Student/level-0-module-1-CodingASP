ea90e096a4a4bbe140aa077085c60dfe3ee995c1
CodingASP <78342170+CodingASP@users.noreply.github.com>
2021-04-12 16:54:00 -0700

    4/12/21

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,25 +1,45 @@
 package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
+
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-int score = 0;
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-5
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		String Riddle1 = JOptionPane.showInputDialog("What can you catch but not throw?");
+		if (Riddle1.equalsIgnoreCase("a cold")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+			
+		}
+	
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong...");
+			JOptionPane.showMessageDialog(null, "The answer is a cold.");
+		}
+	
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
-
+String Riddle2 = JOptionPane.showInputDialog("What is at the end of the rainbow?");
+if (Riddle2.equalsIgnoreCase("The letter w")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score += 1;
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong...");
+	JOptionPane.showMessageDialog(null, "The answer is the letter w.");
+}
 		// 2. Make a pop up to show the score.
-		
+JOptionPane.showMessageDialog(null,"You're score is " + score);
 	}
 }
-
