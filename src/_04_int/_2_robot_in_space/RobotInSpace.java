fb9e0507155f6ad58b5b52774e177e3ab99f8db7
CodingASP <78342170+CodingASP@users.noreply.github.com>
2021-04-19 16:55:29 -0700

    4/19/21

--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -28,17 +28,32 @@ public class RobotInSpace implements KeyEventDispatcher {
 	private void moveRobot(int keyPressed) throws InterruptedException {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
-
-		// 3. If the up arrow is pressed, move the Robot up the screen.
-
-		// 4. If the down arrow is pressed, move the Robot down.
-
-		// 5. If the left arrow is pressed, make the Robot go left.
-
-		// 6. If right is pressed, move the Robot right.
+System.out.println(keyPressed);
+		// 3. Left
+if (keyPressed == 37){
+	rob.setAngle(-90);
+	rob.microMove(2);
+}
+		// 4. Down
+if (keyPressed == 40) {
+	rob.setAngle(-180);
+	rob.microMove(2);
+}
+		// 5. Right
+if (keyPressed == 39) {
+	rob.setAngle(90);
+	rob.microMove(2);
+}
+		// 6. Up
+if (keyPressed == 38) {
+	rob.setAngle(0);
+	rob.microMove(2);
+}
+	}
+	
 
 		// 7. Run your program and move the Robot to R2-D2 for a surprise!
-	}
+	
 
 	private void checkIfR2D2Found() throws Exception {
 		int robotLocationX = rob.getX();
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,11 @@
 
 void setup() {
-
+size (500, 500);
+PImage face = loadImage ("download.jpg");
+face.resize (100, 100); image (face, 100, 100);
 }
 void draw() {
+ellipse (40, 40, 60, 60);
+fill (255, 255, 255);
 
 }
